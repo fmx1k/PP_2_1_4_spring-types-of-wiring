408bc9c9966eddf185184d2a238b396f4706dbd5
fmx1k <officialfmx@yandex.ru>
2023-02-17 14:28:56 +0300

    updated

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,15 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class Rabbit4 {
-    private final Duck5 duck;
 
-    public Rabbit4(Duck5 duck) {
+    @Autowired
+    private Duck5 duck;
+
+    public void setDuck(Duck5 duck) {
         this.duck = duck;
     }
+    public Duck5 getDuck() {
+        return duck;
+    }
+
 
     @Override
     public String toString() {
