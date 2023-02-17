51aba5ae658c6b370c5341c07a73cac8d3ae67d2
fmx1k <officialfmx@yandex.ru>
2023-02-17 14:57:03 +0300

    updated

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,9 +1,6 @@
 package koschei.config;
 
-import koschei.models.Egg6;
-import koschei.models.Island2;
-import koschei.models.Needle7;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +12,11 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Deth8 getDeth() {
+        return new Deth8();
     }
 
 }
